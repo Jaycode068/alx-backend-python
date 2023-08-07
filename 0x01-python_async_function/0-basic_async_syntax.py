aac352fba0d1962c57ff5e2d4303a2436e23fdd4
Jaycode068 <ajayijohnson68@yahoo.com>
2023-08-07 20:18:58 +0000

    An asyn function that genenrates random floatingnnumbers

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,11 @@
-import asyncio, random
+#!/usr/bin/env python3
+
+import asyncio
+import random
 
 async def wait_random(max_delay=10):
-    result = random.uniform(1, max_delay)
+    result = random.uniform(0, max_delay)
+    await asyncio.sleep(result)
     return result
 
 
